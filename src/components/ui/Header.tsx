3a431cf8122285b9616b5759912f08dbfb3cd8d0
Santiago-Andres-Olivera <saoliverav@udistrital.edu.co>
2024-04-17 11:59:08 -0500

    Header Login Logout component part with mock data

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
+import HeaderLoginLogout from './HeaderLoginLogout';
 
 
 
@@ -33,8 +34,8 @@ function Header(): JSX.Element {
         
         {/* For screens medium break point and above span 3, for screens less than the medium
         breakpoint span 6  */}
-        <div className="md:col-span-3 col-span-6 bg-red-200 grid grid-cols-3 ">
-
+        <div className="md:col-span-3 col-span-4 bg-red-200 grid grid-cols-3 ">
+        {/* TTE logo and name */}
             <div className="col-span-1 bg-white  flex justify-end items-center">
               <div className="p-2 relative flex justify-end ">
                 <Image
@@ -61,8 +62,8 @@ function Header(): JSX.Element {
         </div>
         
         
-        <div className="col-span-2 bg-indigo-200  md:grid md:grid-cols-2  hidden ">
-       
+        <div className="col-span-2 bg-white md:grid md:grid-cols-2  hidden items-center">
+        {/* Shop list and wishList */}
             <div className="col-span-1 bg-white ">
                 <p className="text-black text-sm py-3">
                     Shop List
@@ -76,25 +77,26 @@ function Header(): JSX.Element {
             </div>
         </div>
         
-       
-        <div className="md:col-span-4 col-span-3 bg-white flex items-center">
-            <span className="p-2">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-500">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
-            </span>
-            <input 
-              type="text" 
-              placeholder="Search..." 
-              className="p-2 rounded-md text-black flex-grow"
-            />
+       {/* Search bar */}
+        <div className="md:col-span-3 col-span-2 bg-white grid grid-cols-3 items-center gap-2">
+            <div className="md:p-2 md:col-span-1 md:justify-self-center col-span-2 justify-self-end">
+                <span >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-500 ">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+                </span>
+            </div>
+            <div className="p-2 md:col-span-2  rounded-md text-black grid-grow md:block hidden">
+                <input 
+                type="text" 
+                placeholder="Search..." 
+                />
+            </div>
         </div>
         
-       
-        <div className="col-span-3 bg-white flex items-center justify-center">
-            <button className="text-black text-sm py-3">
-                Login
-            </button>
+       {/* Header LoginLogout Component */}
+        <div className="md:col-span-4 col-span-6 bg-white grid grid-flow-col items-center justify-center">
+            <HeaderLoginLogout />
         </div>
       </div>
 
