3852a3f8d7bf20476cf6f7e942e7c4d1e870cf1b
Santiago-Andres-Olivera <saoliverav@udistrital.edu.co>
2024-04-19 09:29:25 -0500

    Header Login Logout modification

--- src/components/ui/HeaderLoginLogout.tsx
+++ src/components/ui/HeaderLoginLogout.tsx
@@ -2,59 +2,58 @@ import React, { useState, useEffect } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
 //import { useRouter } from 'next/router';
 
+interface User {
+    username: string;
+    role: string;
+    // other properties...
+}
+
 function HeaderLoginLogout() : JSX.Element {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [username, setUsername] = useState('');
     const [rol, setRol] = useState('');
     const [isOpen, setIsOpen] = useState(false);
+    const [user, setUser] = useState<User | null>(null);
+    const [token, setToken] = useState<string | null>(null);
     //const router = useRouter();
 
     // Check if user data is in local storage to keep user logged in across sessions
     useEffect(() => {
-        const storedUser = localStorage.getItem('user');
-        if (storedUser) {
-            const user = JSON.parse(storedUser);
-            setIsLoggedIn(true);
-            setUsername(user.username);
-        }
+    const storedToken = localStorage.getItem('token') || sessionStorage.getItem('token');
+        setToken(storedToken);
     }, []);
 
-    const mockLogin = () => {
-        const mockResponse = {
-            token: "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiRU1QTE9ZRUUiLCJzdWIiOiJlbXBsb3llZWp1YW5AbWFpbC5jb20iLCJpYXQiOjE3MTMyODgwMTMsImV4cCI6MTcxMzI4OTUxM30.SZadVzE-JHX68dsqxi59s2c8TnDI6oqPcuIsKH7Aw68",
-            email: "employeeSantiago@mail.com",
-            username: "Santiago",
-            rol: "employee"
-        };
-        localStorage.setItem('user', JSON.stringify(mockResponse));
-        setIsLoggedIn(true);
-        setUsername(mockResponse.username);
-        setRol(mockResponse.rol);
-    };
+    useEffect(() => {
+        const username = localStorage.getItem('username') || sessionStorage.getItem('username');
+        if (token) {
+            const userData = JSON.parse(atob(token.split('.')[1]));
+            setUser({ ...userData, username });
+        }
+    }, [token]);
 
     const handleLogout = () => {
-        setIsLoggedIn(false);
-        setUsername('');
-        localStorage.removeItem('user');
-        //  redirect to home page
-        //router.push('/');
-
+        localStorage.removeItem('token');
+        sessionStorage.removeItem('token');
+        setToken(null);
+        setUser(null);
     };
 
     return (
         <div >
-            {isLoggedIn ? (
+            {user ? (
                 <div>
                     <div className="sm:grid grid-flow-col gap-4 items-center hidden">
-                        <span>{username}</span>
+                        <span>{user?.username}</span>
                         <FaShoppingCart />
-                        <span className='hidden sm:block'>{rol}</span>
+                        <span className='hidden sm:block'>{user?.role}</span>
                         <button className="text-black text-sm py-3" onClick={handleLogout}>
                             Logout
                         </button>
-                        <button className="bg-black text-white text-sm p-1 rounded">
-                            Employee Portal
-                        </button>
+                        {user?.role === 'EMPLOYEE' && (
+                            <button className="bg-black text-white text-sm p-1 rounded">
+                                Employee Portal
+                            </button>
+                        )}
                     </div> 
                     {/* For small screens  */}
                     <div className='sm:hidden group flex flex-col relative '>
@@ -62,18 +61,20 @@ function HeaderLoginLogout() : JSX.Element {
                             className='flex gap-2 items-center cursor-pointer' 
                             onClick={() => setIsOpen(!isOpen)}
                         >
-                            <span>{username}</span>
+                            <span>{user.username}</span>
                             <FaShoppingCart />
                         </div>
                         <nav className={`flex-col gap-4 ${isOpen ? 'block' : 'hidden'} absolute bg-white z-10 top-full`}>
-                            <span>{rol}</span>
+                            <span>{user.role}</span>
                             <div className="flex flex-col gap-1">
                                 <button className="bg-black text-white text-sm p-1 rounded" onClick={handleLogout}>
                                     Logout
                                 </button>
-                                <button className="bg-black text-white text-sm p-1 rounded">
-                                    Employee Portal
-                                </button>
+                                {user?.role === 'EMPLOYEE' && (
+                                    <button className="bg-black text-white text-sm p-1 rounded">
+                                        Employee Portal
+                                    </button>
+                                )}
                             </div>
                         </nav>
                     </div>         
@@ -81,7 +82,7 @@ function HeaderLoginLogout() : JSX.Element {
 
                 
             ) : (
-                <button className="text-black text-sm py-3" onClick={mockLogin}>
+                <button className="text-black text-sm py-3" onClick={() => window.location.href = '/login'}>
                     Login
                 </button>
             )}
